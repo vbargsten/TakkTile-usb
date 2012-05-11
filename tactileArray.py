a4d9a776a5856fc13af53ceb67de74b6f2f65a2d
Ian Daniher <it.daniher@gmail.com>
2012-05-11 18:28:43 -0400

    added getDataRawSeq and statistics to tactileArray.py

--- tactileArray.py
+++ tactileArray.py
@@ -4,11 +4,6 @@ import usb
 import numpy
 import time
 
-
-#self.dev.ctrl_transfer(0x40|0x80, 0x6C, 0, 16, 12)
-
-
-
 class Tactile:
 	def __init__(self):
 		self.dev = usb.core.find(idVendor=0x59e3, idProduct=0x74C7)
@@ -27,9 +22,20 @@ class Tactile:
 
 	def getDataRaw(self, row):
 		"""return an array of five integers between 0 and 1023, matching the 10b sample depth of the sensors."""
+		data = self.dev.ctrl_transfer(0x40|0x80, 0x7C, 0, row, 20)
+		data = numpy.resize(data, (5,4))
+		#print [map(hex, datum[0:2]) for datum in data]
+		temperature = [((datum[3] >> 6| datum[2] << 2)) for datum in data]
+		data = [((datum[1] >> 6| datum[0] << 2)) for datum in data]
+		self.rawData[row] = data
+		# return the 1x5 array
+		return data, temperature
+
+	def getDataRawSeq(self, row):
+		"""return an array of five integers between 0 and 1023, matching the 10b sample depth of the sensors."""
 		data = self.dev.ctrl_transfer(0x40|0x80, 0x8C, 0, row, 20)
 		data = numpy.resize(data, (5,4))
-#		print [map(hex, datum[0:2]) for datum in data]
+		#print [map(hex, datum[0:2]) for datum in data]
 		temperature = [((datum[3] >> 6| datum[2] << 2)) for datum in data]
 		data = [((datum[1] >> 6| datum[0] << 2)) for datum in data]
 		self.rawData[row] = data
@@ -52,7 +58,10 @@ class Tactile:
 		return self.calibrationData[row][column]
 
 if __name__ == "__main__":
-	import sys
 	tact = Tactile()
-	while True:
-		print tact.getDataRaw(1)
+	shotgun = [ tact.getDataRaw(1)[0] for sample in range(1000) ]
+	shotgun = numpy.rollaxis(numpy.array(shotgun), 1)
+	sequential = [ tact.getDataRawSeq(1)[0] for sample in range(1000) ]
+	sequential = numpy.rollaxis(numpy.array(sequential), 1)
+	print numpy.std(sequential[0]), numpy.std(shotgun[0])
+	print numpy.mean(sequential[0]), numpy.mean(shotgun[0])	
