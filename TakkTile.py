6617d8a1babdc8bf27536d169970e31cdbcecb5a
Ian Daniher <it.daniher@gmail.com>
2012-07-29 23:39:14 -0400

    reworked TakkTile class to use 1D datastructures wherever possible - getData now returns a dictionary of indexes to values

--- TakkTile.py
+++ TakkTile.py
@@ -5,13 +5,16 @@
 
 import usb
 import re
+import itertools
 
 _unTwos = lambda x, bitlen: x-(1<<bitlen) if (x&(1<<(bitlen-1))) else x
 _chunk = lambda l, x: [l[i:i+x] for i in xrange(0, len(l), x)]
+_flatten = lambda l: list(itertools.chain(*[[x] if type(x) not in [list] else x for x in l]))
 
 class TakkTile:
 
 	_getTinyAddressFromRowColumn = lambda self, row, column: (((row)&0x0F) << 4 | (column&0x07) << 1)
+	_getTinyAddressFromIndex = lambda self, index: (((index/5)&0x0F) << 4 | ((index%5)&0x07) << 1)
 	exists = lambda self: bool(self.arrayID)^1
 
 	def __init__(self, arrayID = 0):
@@ -24,7 +27,7 @@ class TakkTile:
 		# populates bitmap of live sensors
 		self.alive = self.getAlive()
 		# retrieve calibration bytes and calculate the polynomial's coefficients
-		self.calibrationCoefficients = [[self.getCalibrationCoefficients(row, column) for column in range(5)] for row in range(8)]
+		self.calibrationCoefficients = dict(zip(self.alive, [self.getCalibrationCoefficients(index) for index in self.alive]))
 		self.UID = self.dev.ctrl_transfer(0x80, usb.REQ_GET_DESCRIPTOR, 
 			(usb.util.DESC_TYPE_STRING << 8) | self.dev.iSerialNumber, 0, 255)[2::].tostring().decode('utf-16')
 
@@ -36,11 +39,10 @@ class TakkTile:
 		return [match.span()[0] for match in re.finditer('1', bitmap)] 
 	
 
-	def getCalibrationCoefficients(self, row, column):
+	def getCalibrationCoefficients(self, index):
 		""" This function implements the compensation & calibration coefficient calculations from page 15 of AN3785. """
-		# iterate through rows and columns
 		# get calibration data from a specified location
-		cd = self.getCalibrationData(row, column)  
+		cd = self.getCalibrationData(index)  
 		# define short alias to save me keystrokes & enhance readability
 		cc = {"a0":0, "b1":0, "b2":0, "c12":0, "c11":0, "c22":0}
 		# cell not alive
@@ -63,45 +65,47 @@ class TakkTile:
 		return cc
 
 
-	def getDataRaw(self, row):
+	def getDataRaw(self):
 		"""Query the TakkTile USB interface for the pressure and temperature samples from a specified row of sensors.."""
+		temperature = []
+		pressure = []
 		# 0x7C is "get data" vendor request, takes a row as a wValue, returns 20 bytes
-		data = self.dev.ctrl_transfer(0x40|0x80, 0x7C, 0, row, 20)
-		# use _chunk to shape the 20 bytes into five chunks of four bytes each
-		data = _chunk(data, 4)
-		# temperature is contained in the last two bytes of each four byte chunk, pressure in the first two
-		# each ten bit number is encoded in two bytes, MSB first, zero padded / left alligned
-		temperature = [_unTwos((datum[3] >> 6| datum[2] << 2), 10) for datum in data]
-		data = [_unTwos((datum[1] >> 6| datum[0] << 2), 10) for datum in data]
-		# fix overflow problems
-		return data, temperature
+		for ID in range(2,40,5):
+			if ID in self.alive:
+				data = _chunk(self.dev.ctrl_transfer(0x40|0x80, 0x7C, 0, ID/5, 20), 4)
+				# temperature is contained in the last two bytes of each four byte chunk, pressure in the first two
+				# each ten bit number is encoded in two bytes, MSB first, zero padded / left alligned
+				temperature += [_unTwos((datum[3] >> 6| datum[2] << 2), 10) for datum in data if datum.count(0) != 4]
+				pressure += [_unTwos((datum[1] >> 6| datum[0] << 2), 10) for datum in data if datum.count(0) != 4]
+		return dict(zip(self.alive, zip(pressure, temperature)))
 
-	def getData(self, row):
+	def getData(self):
 		"""Return measured pressure in kPa, temperature compensated and factory calibrated."""
 		# get raw 10b data
-		Padc, Tadc = self.getDataRaw(row)
-		print Padc, Tadc
+		data = self.getDataRaw()
+		Padc = lambda cell: data[cell][0]
+		Tadc = lambda cell: data[cell][1]
 		# initialize array for compensated pressure readings
-		Pcomp = len(Padc)*[0]
+		Pcomp = {}
 		# for element in the returned pressure data...
-		for column in range(len(Padc)):
+		for cell in self.alive:
 			# load the calibration coefficients calculated when the TakkTile class is initialized
-			cc = self.calibrationCoefficients[row][column]
+			cc = self.calibrationCoefficients[cell]
 			# apply the formula contained on page 13 of Freescale's AN3785
 			# "The 10-bit compensated pressure output for MPL115A, Pcomp, is calculated as follows: 
 			#  Pcomp = a0 + (b1 + c11*Padc + c12*Tadc) * Padc + (b2 + c22*Tadc) * Tadc"
-			Pcomp[column] = cc["a0"] + (cc["b1"] + cc["c11"]*Padc[column] + cc["c12"]*Tadc[column])*Padc[column] + (cc["b2"] + cc["c22"]*Tadc[column])*Tadc[column]
+			Pcomp[cell] = cc["a0"] + (cc["b1"] + cc["c11"]*Padc(cell) + cc["c12"]*Tadc(cell))*Padc(cell) + (cc["b2"] + cc["c22"]*Tadc(cell))*Tadc(cell)
 			# convert from 10b number to kPa
-			Pcomp[column] = 65.0/1023.0*Pcomp[column]+50
+			Pcomp[cell] = 65.0/1023.0*Pcomp[cell]+50
 			# round to keep sane sigfig count
-			Pcomp[column] = round(Pcomp[column], 4)
+			Pcomp[cell] = round(Pcomp[cell], 4)
 		return Pcomp 
 	
-	def getCalibrationData(self, row, column):
-		"""Request the 12 calibration bytes from a sensor at a specified row and column."""
+	def getCalibrationData(self, index):
+		"""Request the 12 calibration bytes from a sensor at a specified index."""
 		# get the attiny's virtual address for the specified row/column
 		# read the calibration data via vendor request and return it 
-		return self.dev.ctrl_transfer(0x40|0x80, 0x6C, column, row, 12)	
+		return self.dev.ctrl_transfer(0x40|0x80, 0x6C, index%5, index/5, 12)	
 
 if __name__ == "__main__":
 	tact = TakkTile()
@@ -110,7 +114,7 @@ if __name__ == "__main__":
 	import time
 	while True:
 		start = time.time()
-		data = tact.getData(3)
+		data = tact.getData()
 		end = time.time()
 		print round(end-start, 6), data
 		time.sleep(.005)
