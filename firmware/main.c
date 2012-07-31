cc4180c35a7b74d5e5d1c9a944de9b7a4f262c0e
Ian Daniher <it.daniher@gmail.com>
2012-07-31 09:20:36 -0400

    cleaned and commented code. no substantive changes

--- firmware/main.c
+++ firmware/main.c
@@ -1,18 +1,25 @@
 // (C) 2012 Biorobotics Lab and Nonolith Labs
 // (C) 2011, 2012 Ian Daniher (Nonolith Labs) <ian@nonolithlabs.com>
-// (C) 2011 Kevin Mehall (Nonolith Labs) <km@kevinmehall.net>
 // Licensed under the terms of the GNU GPLv3+
 
 #include "TakkTile.h"
 
 #include <avr/eeprom.h>
 
+// run I2C at 1MHz
 #define F_TWI    1000000
 #define TWI_BAUD ((F_CPU / (2 * F_TWI)) - 5) 
 
+// equation for calculating I2C address from row and column
 inline uint8_t calcTinyAddr(uint8_t row, uint8_t column) { return (((row)&0x0F) << 4 | (column&0x07) << 1); }
 
+
 uint8_t botherAddress(uint8_t address, bool stop){
+	/* Function to write address byte to I2C, returns 1 if ACK, 0 if NACK. 
+	'stop' specifies an optional stop bit on the transaction.
+	NB: Don't read from a non-existant address */
+
+	// quick command mode - RIF/WIF trips on ACK
 	TWIC.MASTER.CTRLB |= TWI_MASTER_QCEN_bm;
 	// set address to bother
 	TWIC.MASTER.ADDR = address;
@@ -20,28 +27,35 @@ uint8_t botherAddress(uint8_t address, bool stop){
 	if (address & 1) while(!(TWIC.MASTER.STATUS&TWI_MASTER_RIF_bm));
 	// if address ends in zero, wait for a write to finish
 	else while(!(TWIC.MASTER.STATUS&TWI_MASTER_WIF_bm));
+	// initiate stop condition if (stop)
 	if (stop) TWIC.MASTER.CTRLC |= TWI_MASTER_CMD_STOP_gc;
 	// return 1 if ACK, 0 if NACK
 	return ((TWIC.MASTER.STATUS & TWI_MASTER_RXACK_bm) >> 4)^1; 
 }
 
 inline void startConversion(){
-	// enable all MPL115A2s
+	/* Initiates the analog-to-digital conversion of pressure and temperature
+	on all MPL115A2 sensors on all attached rows. */
+
+	// enable all MPL115A2 by writing to 0x0C
 	uint8_t ACK = botherAddress(calcTinyAddr(0, 6), 1);
 	// write address byte of MPL115A2
 	botherAddress(0xC0, 0);
-	// write 1 to 0x12 - start conversion of pressure & temperature
+	// write 0x01 to 0x12 - start conversion of pressure & temperature
 	TWIC.MASTER.DATA = 0x12;
 	while(!(TWIC.MASTER.STATUS&TWI_MASTER_WIF_bm));
 	TWIC.MASTER.DATA = 0x01;
 	while(!(TWIC.MASTER.STATUS&TWI_MASTER_WIF_bm));
 	// end transaction
 	TWIC.MASTER.CTRLC |= TWI_MASTER_CMD_STOP_gc;
-	// disable all MPL115A2s
+	// if you got an ACK on enable, disable all the MPL115A2s
 	if (ACK == 1) botherAddress(calcTinyAddr(0, 6)^1, 1);
 }
 
 void getCalibrationData(void){
+	/* Iterate through all rows and all columns. If that cell is alive,
+	read 12 calibration bytes from 0x04 into calibrationData. */
+
 	for (uint8_t row = 0; row < 8; row++) {
 		for (uint8_t column = 0; column < 5; column++) {
 			TWIC.MASTER.CTRLC &= ~TWI_MASTER_ACKACT_bm;
@@ -63,7 +77,8 @@ void getCalibrationData(void){
 				TWIC.MASTER.ADDR = 0xC1;
 				while(!(TWIC.MASTER.STATUS&TWI_MASTER_RIF_bm));
 				for (uint8_t byteCt = 0; byteCt < 12; byteCt++){
-					calibrationData[60*row+12*column+byteCt] = TWIC.MASTER.DATA;
+					uint8_t index = 60*row+12*column+byteCt;
+					calibrationData[index] = TWIC.MASTER.DATA;
 					// if transaction isn't over, wait for ACK
 					if (byteCt < 11) while(!(TWIC.MASTER.STATUS&TWI_MASTER_RIF_bm));
 					// if transaction is almost over, set next byte to NACK
@@ -77,6 +92,9 @@ void getCalibrationData(void){
 }
 
 void getSensorData(void){
+	/* Iterate through all rows and all columns. If that cell is alive,
+	read four data bytes from 0x00 into sensorData */
+
 	for (uint8_t row = 0; row < 8; row++) {
 		for (uint8_t column = 0; column < 5; column++) {
 			TWIC.MASTER.CTRLC &= ~TWI_MASTER_ACKACT_bm;
@@ -102,7 +120,7 @@ void getSensorData(void){
 					uint8_t index = byteCt + column*4 + row*20;
 					sensorData[index] = TWIC.MASTER.DATA;
 					// if transaction isn't over, wait for ACK
-						if (byteCt < 3) while(!(TWIC.MASTER.STATUS&TWI_MASTER_RIF_bm));
+					if (byteCt < 3) while(!(TWIC.MASTER.STATUS&TWI_MASTER_RIF_bm));
 					// if transaction is almost over, set next byte to NACK
 					if (byteCt == 2) TWIC.MASTER.CTRLC |= TWI_MASTER_ACKACT_bm | TWI_MASTER_CMD_STOP_gc;
 				}
@@ -115,6 +133,8 @@ void getSensorData(void){
 
 
 void getAlive(void){
+	/* Iterate through rows and columns, generating a bitmap of alive sensors. */
+
 	for (uint8_t row = 0; row < 8; row++) {
 		uint8_t sensor_bm = 0;
 		for (uint8_t column = 0; column < 5; column++) {
@@ -133,6 +153,9 @@ void getAlive(void){
 }
 
 ISR(TCC0_CCA_vect){
+	/* Timer interrupt that trips 1ms after TCC0.CNT is set to 0.
+	Change the LED state, clock out all data from all alive sensors, and start next conversion. */
+
     PORTR.OUTTGL = 1 << 1;
 	getSensorData();
 	startConversion();
@@ -194,13 +217,17 @@ bool EVENT_USB_Device_ControlRequest(USB_Request_Header_t* req){
 				}
 				
 				return true;
-				
-			case 0xBA: // bother a specified I2C address, return '1' if address ACKs, '0' if NACK
+
+			// bother a specified I2C address, return '1' if address ACKs, '0' if NACK
+			// mnemonic - 0xBotherAddress
+			case 0xBA: 
 				ep0_buf_in[0] = botherAddress(req->wIndex, req->wValue);
 				USB_ep0_send(1);
 				return true;
 
-			case 0x5C: // return bitmap of alive rows
+			// return a bitmap of alive cells 
+			// mnemonic - 0x5Can
+			case 0x5C: 
 				for (uint8_t i = 0; i < 8; i++) {ep0_buf_in[i] = bitmap[i];}
 				USB_ep0_send(8);
 				return true;
@@ -223,18 +250,21 @@ bool EVENT_USB_Device_ControlRequest(USB_Request_Header_t* req){
 				return true;
 				}
 
-			case 0xE0: // Read EEPROM
+			// read EEPROM	
+			case 0xE0: 
 				eeprom_read_block(ep0_buf_in, (void*)(req->wIndex*64), 64);
 				USB_ep0_send(64);
 				return true;
-				
-			case 0xE1: // Write EEPROM
+
+			// write EEPROM	
+			case 0xE1: 
 				usb_cmd = req->bRequest;
 				cmd_data = req->wIndex;
 				USB_ep0_send(0);
 				return true; // Wait for OUT data (expecting an OUT transfer)
-				
-			case 0xBB: // disconnect from USB, jump to bootloader
+
+			// disconnect from USB, jump to bootloader	
+			case 0xBB: 
 				USB_enter_bootloader();
 				return true;
 		}
