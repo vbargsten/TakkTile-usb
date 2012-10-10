924f4073f52301b9b81011c99b0cd9074d8f2741
Leif <emp@plutarch.(none)>
2012-10-09 21:37:25 -0400

    couple EVENT_USB_Device_ControlRequest to status LED

--- firmware/TakkTile.h
+++ firmware/TakkTile.h
@@ -17,3 +17,5 @@ void configHardware(void);
 void startConversion(void);
 void getRowData(uint8_t row, uint8_t *dataOut);
 bool EVENT_USB_Device_ControlRequest(USB_Request_Header_t* req);
+
+inline void EVENT_USB_Device_ConfigurationChanged(uint8_t configuration) {PORTR.OUTSET = 1 << 1;} 
