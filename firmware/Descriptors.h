ca5de369b1f046bed80cf1f55069087d8f813ed5
Ian Daniher <it.daniher@gmail.com>
2012-11-07 17:33:49 -0500

    start cleanup of Descriptors.h

--- firmware/Descriptors.h
+++ firmware/Descriptors.h
@@ -50,7 +50,7 @@
 		{
 			USB_Descriptor_Configuration_Header_t Config;
 			
-			USB_Descriptor_Interface_t            XmegaExampleInterface;
+			USB_Descriptor_Interface_t            TakkTileInterface;
 			USB_Descriptor_Endpoint_t              Test_DataInEndpoint;
 			USB_Descriptor_Endpoint_t              Test_DataOutEndpoint;
 		} USB_Descriptor_Configuration_t;
